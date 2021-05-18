ac6c89a039193bd7e3a5560ca62a89fa9a19ad99
Mateus Canelhas <mateus.canelhas@gmail.com>
2021-05-17 23:14:11 -0300

    Bugfix to Exception super() Chain

--- src/main/java/canelhas/cars/common/exception/CarsException.java
+++ src/main/java/canelhas/cars/common/exception/CarsException.java
@@ -6,11 +6,12 @@ import org.springframework.http.HttpStatus;
 public abstract class CarsException extends RuntimeException {
 
     protected CarsException( String message ) {
-
+        super( message );
     }
 
 
     protected CarsException( String message, HttpStatus httpStatus ) {
+        super( message );
     }
 
     public abstract HttpStatus getStatus( );
