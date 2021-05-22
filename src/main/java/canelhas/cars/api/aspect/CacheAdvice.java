dd49c5f981f910a29c11a7744ffe07771e218c7f
Mateus Canelhas <mateus.canelhas@gmail.com>
2021-05-22 02:54:23 -0300

    Better Annotations

--- src/main/java/canelhas/cars/api/aspect/CacheAdvice.java
+++ src/main/java/canelhas/cars/api/aspect/CacheAdvice.java
@@ -1,4 +1,15 @@
 package canelhas.cars.api.aspect;
 
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class CacheAdvice {
+
+    @Before( "execution(public * *.*(.., @canelhas.cars.api.other.domain.LRU ( * ),..))" )
+    public void lookupCache( JoinPoint joinPoint ) {}
 }
--- src/main/java/canelhas/cars/api/other/domain/LRU.java
+++ src/main/java/canelhas/cars/api/other/domain/LRU.java
@@ -1,4 +1,16 @@
 package canelhas.cars.api.other.domain;
 
+import org.aspectj.lang.annotation.Pointcut;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention( RetentionPolicy.RUNTIME)
+@Target( { ElementType.PARAMETER })
 public @interface LRU {
+
+
+
 }
